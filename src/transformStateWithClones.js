f7f42283c8ef4d80b4ff4a3b9f858d959e63f4f5
Ivan Herhi <ionherhi@gmail.com>
2024-12-26 10:50:32 +0100

    Clones-1

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -5,9 +5,63 @@
  * @param {Object[]} actions
  *
  * @return {Object[]}
+ *
+ * transformStateWithClones()
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        result.push(addProperties(state, action.extraData));
+        break;
+
+      case 'removeProperties':
+        result.push(removeProperties(state, action.keysToRemove));
+        break;
+
+      case 'clear':
+        result.push(clear(state));
+        break;
+    }
+  }
+
+  return result;
+}
+
+function addProperties(state, extraData) {
+  const stateA = {};
+
+  Object.assign(stateA, state, extraData);
+
+  return stateA;
+}
+
+function removeProperties(state, keysToRemove) {
+  /* const stateB = {};
+
+  for (const key of keysToRemove) {
+    delete state[key];
+  }
+
+  Object.assign(stateB, state);
+
+  return stateB; */
+
+  const stateB = { ...state };
+
+  for (const key of keysToRemove) {
+    delete stateB[key];
+  }
+
+  return stateB;
+}
+
+function clear(state) {
+  const emptyObject = {};
+
+  return emptyObject;
 }
 
 module.exports = transformStateWithClones;
